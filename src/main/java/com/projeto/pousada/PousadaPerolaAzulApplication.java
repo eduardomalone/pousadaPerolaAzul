25a61f771f9b391dc4a97c668d6204d4ca14cac0
Eduardo Lucio <eduardomalone@hotmail.com>
2020-04-11 20:41:36 -0300

    primeiro commit

---
+++ src/main/java/com/projeto/pousada/PousadaPerolaAzulApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.pousada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PousadaPerolaAzulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PousadaPerolaAzulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/pousada/PousadaPerolaAzulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.pousada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PousadaPerolaAzulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

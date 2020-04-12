507f5ac921b209bae10fe054c9267854fbbe160b
Eduardo Lucio <eduardomalone@hotmail.com>
2020-04-12 01:56:57 -0300

    base de dados m2 e entidade funcionario

--- src/main/java/com/projeto/pousada/domain/Funcionario.java
+++ src/main/java/com/projeto/pousada/domain/Funcionario.java
@@ -3,31 +3,38 @@ package com.projeto.pousada.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Getter
 @Setter
+@Entity
 public class Funcionario implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer codigo;
     private String nome;
     private String cargo;
     private double salario;
-    private String cpj;
+    private String cpf;
     private String telefone;
 
     public Funcionario(){};
 
-    public Funcionario(Integer codigo, String nome, String cargo, double salario, String cpj, String telefone) {
+    public Funcionario(Integer codigo, String nome, String cargo, double salario, String cpf, String telefone) {
 
         this.codigo = codigo;
         this.nome = nome;
         this.cargo = cargo;
         this.salario = salario;
-        this.cpj = cpj;
+        this.cpf = cpf;
         this.telefone = telefone;
     }
 

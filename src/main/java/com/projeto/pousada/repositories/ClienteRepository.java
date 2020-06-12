33daccc52a7a0e5ef2c5bb9da0e4022936071a45
Eduardo Lucio <eduardomalone@hotmail.com>
2020-06-12 01:03:18 -0300

    validacao de email

--- src/main/java/com/projeto/pousada/repositories/ClienteRepository.java
+++ src/main/java/com/projeto/pousada/repositories/ClienteRepository.java
@@ -1,10 +1,14 @@
 package com.projeto.pousada.repositories;
 
 import com.projeto.pousada.domain.Cliente;
-import com.projeto.pousada.domain.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 }
--- src/main/java/com/projeto/pousada/services/validantions/ClienteInsertionValidator.java
+++ src/main/java/com/projeto/pousada/services/validantions/ClienteInsertionValidator.java
@@ -1,9 +1,12 @@
 package com.projeto.pousada.services.validantions;
 
+import com.projeto.pousada.domain.Cliente;
 import com.projeto.pousada.domain.enums.TipoCliente;
 import com.projeto.pousada.dto.ClienteNewDTO;
+import com.projeto.pousada.repositories.ClienteRepository;
 import com.projeto.pousada.resources.exceptions.FieldMessage;
 import com.projeto.pousada.services.validantions.util.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -12,6 +15,9 @@ import java.util.List;
 
 public class ClienteInsertionValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+    @Autowired
+    private ClienteRepository repository;
+
     @Override
     public void initialize(ClienteInsert ann) {
     }
@@ -19,7 +25,7 @@ public class ClienteInsertionValidator implements ConstraintValidator<ClienteIns
     public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
         List<FieldMessage> list = new ArrayList<>();
 
-        // inclua os testes aqui, inserindo erros na lista
+        // inclui os testes aqui, inserindo erros na lista
         if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) &&
                 !BR.isValidCPF(objDto.getCpfCnpj())){
             list.add(new FieldMessage("cpfCnpj","CPF invalido"));
@@ -28,6 +34,12 @@ public class ClienteInsertionValidator implements ConstraintValidator<ClienteIns
         if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) &&
                 !BR.isValidCNPJ(objDto.getCpfCnpj())){
             list.add(new FieldMessage("cpfCnpj","CNPJ invalido"));
+        }
+
+        Cliente aux = repository.findByEmail(objDto.getEmail());
+        if(aux != null){
+            list.add(new FieldMessage("email", "Email ja cadastrado!"));
+
         }
 
         for (FieldMessage e : list) {
